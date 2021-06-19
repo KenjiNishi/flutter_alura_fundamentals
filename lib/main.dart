2038cbe5363831efc62fbe3ad144b6b214dc9a59
KenjiNishi <wk.nishizawa@gmail.com>
2021-06-19 18:13:46 -0300

    Lecture 3 - Forms, toString, snackbar alert

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,93 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: TransferList(),
-      appBar: AppBar(
-        title: Text("Transfers"),
+  runApp(AppRoot());
+}
+
+class AppRoot extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: TransferForm(),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () => {},
-      ),
-    ),
-  ));
+    );
+  }
+}
+
+class TransferForm extends StatelessWidget {
+  final TextEditingController _accountNumberController =
+      new TextEditingController();
+  final TextEditingController _valueController = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("New transfer"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _accountNumberController,
+                style: TextStyle(fontSize: 24.0),
+                decoration: InputDecoration(
+                    labelText: 'Account Number', hintText: '0000'),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _valueController,
+                style: TextStyle(fontSize: 24.0),
+                decoration: InputDecoration(
+                    icon: Icon(Icons.monetization_on),
+                    labelText: 'Value to transfer',
+                    hintText: '999'),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            ElevatedButton(
+                child: Text("Transfer!"),
+                onPressed: () {
+                  debugPrint("Transfer button pressed!");
+                  final value = int.tryParse(_valueController.text);
+                  final String account = _accountNumberController.text;
+                  if (value != null) {
+                    final createdTransfer = Transfer(value, account);
+                    debugPrint('$createdTransfer');
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('$createdTransfer'),
+                      ),
+                    );
+                  }
+                }),
+          ],
+        ));
+  }
 }
 
 class TransferList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TransferItem(Transfer(105, "12356")),
-        TransferItem(Transfer(210, "12356")),
-        TransferItem(Transfer(420, "12356")),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Transfers"),
+      ),
+      body: Column(
+        children: <Widget>[
+          TransferItem(Transfer(105, "12356")),
+          TransferItem(Transfer(210, "12356")),
+          TransferItem(Transfer(420, "12356")),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => {},
+      ),
     );
   }
 }
@@ -50,4 +114,9 @@ class Transfer {
   final String accountNumber;
 
   Transfer(this.value, this.accountNumber);
+
+  @override
+  String toString() {
+    return 'Transfer{value:$value, accountNumber:$accountNumber}';
+  }
 }
