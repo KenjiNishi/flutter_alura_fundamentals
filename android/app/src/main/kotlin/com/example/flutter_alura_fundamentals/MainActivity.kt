2b3796ec4de64b48b3451f08a964ab7a9229f739
KenjiNishi <wk.nishizawa@gmail.com>
2021-06-19 16:48:45 -0300

    first lecture

---
+++ android/app/src/main/kotlin/com/example/flutter_alura_fundamentals/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_alura_fundamentals
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Column(
+    children: const <Widget>[
+      Text(
+        'My first Flutter app',
+        textDirection: TextDirection.ltr,
+      ),
+      Text(
+        'Craft beautiful UIs',
+        textDirection: TextDirection.ltr,
+      ),
+    ],
+  ));
+}
