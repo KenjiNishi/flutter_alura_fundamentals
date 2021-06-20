acf1105cd0b87eff8840fb50622fbcafff3bac77
KenjiNishi <wk.nishizawa@gmail.com>
2021-06-20 00:33:01 -0300

    lecture 2 - implementing flux, events on containers, change widget size

--- course2_sqflite/lib/screens/contacts_form.dart
+++ course2_sqflite/lib/screens/contacts_form.dart
@@ -1,6 +1,16 @@
+import 'package:course2_sqflite/models/contact.dart';
 import 'package:flutter/material.dart';
 
-class ContactForm extends StatelessWidget {
+class ContactForm extends StatefulWidget {
+  @override
+  _ContactFormState createState() => _ContactFormState();
+}
+
+class _ContactFormState extends State<ContactForm> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _accountNumberController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,12 +22,14 @@ class ContactForm extends StatelessWidget {
         child: Column(
           children: [
             TextField(
+              controller: _nameController,
               decoration: InputDecoration(labelText: 'Full name'),
               style: TextStyle(fontSize: 24),
             ),
             Padding(
               padding: const EdgeInsets.only(top: 8),
               child: TextField(
+                controller: _accountNumberController,
                 decoration: InputDecoration(labelText: 'AccountNumber'),
                 style: TextStyle(fontSize: 18),
                 keyboardType: TextInputType.number,
@@ -27,7 +39,15 @@ class ContactForm extends StatelessWidget {
               padding: const EdgeInsets.only(top: 16),
               child: SizedBox(
                 width: double.maxFinite,
-                child: ElevatedButton(onPressed: () {}, child: Text('Create')),
+                child: ElevatedButton(
+                    onPressed: () {
+                      final String name = _nameController.text;
+                      final accNum =
+                          int.tryParse(_accountNumberController.text);
+                      final Contact newContact = Contact(name, accNum!);
+                      Navigator.pop(context, newContact);
+                    },
+                    child: Text('Create')),
               ),
             )
           ],
