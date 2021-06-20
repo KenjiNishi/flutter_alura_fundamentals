6b200c8983c0c364c43a9f368672e90c5bee894f
KenjiNishi <wk.nishizawa@gmail.com>
2021-06-20 00:04:48 -0300

    using InkWell instead of GestureDetector

--- course2_sqflite/lib/screens/dashboard.dart
+++ course2_sqflite/lib/screens/dashboard.dart
@@ -16,33 +16,35 @@ class Dashboard extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.all(8),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => ContactsList()));
-              },
-              child: Container(
-                padding: EdgeInsets.all(8),
-                color: Colors.amber,
-                height: 80,
-                width: 140,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Icon(
-                      Icons.people,
-                      color: Colors.black,
-                      size: 38,
-                    ),
-                    Text(
-                      'Contacts',
-                      style: TextStyle(
+            child: Material(
+              color: Theme.of(context).primaryColor,
+              child: InkWell(
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => ContactsList()));
+                },
+                child: Container(
+                  padding: EdgeInsets.all(8),
+                  height: 80,
+                  width: 140,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Icon(
+                        Icons.people,
                         color: Colors.black,
-                        fontSize: 16,
+                        size: 38,
                       ),
-                    ),
-                  ],
+                      Text(
+                        'Contacts',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
