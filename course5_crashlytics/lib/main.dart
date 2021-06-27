85156379394c5671b7eb1dfab68df4ce4f74dc2c
KenjiNishi <wk.nishizawa@gmail.com>
2021-06-27 14:39:04 -0300

    lecture1 - introduction and configuring fireabase and crashanalytics

--- course5_crashlytics/lib/main.dart
+++ course5_crashlytics/lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:alura_crashlytics/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
   runApp(BytebankApp());
 }
 
 class BytebankApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
