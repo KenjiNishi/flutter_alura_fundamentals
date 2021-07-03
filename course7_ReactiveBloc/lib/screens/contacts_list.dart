ea4113c6db29df0d525f421a9e609e90a91503a3
KenjiNishi <wk.nishizawa@gmail.com>
2021-07-03 15:11:29 -0300

    lecture4 - async, states, bloc and forms

--- course7_ReactiveBloc/lib/screens/contacts_list.dart
+++ course7_ReactiveBloc/lib/screens/contacts_list.dart
@@ -87,11 +87,7 @@ class _ContactsListState extends State<ContactsList> {
                 return _ContactItem(
                   contact,
                   onClick: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => TransactionForm(contact),
-                      ),
-                    );
+                    push(context, TransactionFormContainer(contact));
                   },
                 );
               },
